f8da1401632a36b90e31d7915b17a46aa2850edc
RosaMedina28 <rosamedina1008@gmail.com>
2022-02-28 17:54:18 -0600

    olo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,66 @@
 <template>
-  <div class="hello">
+  <div class="">
+  <div class="altura">
+    <div class="nav">
+    <nav class="navbar navbar-expand-sm ">
+       <div class="container-fluid">
+    <span class="navbar-text">H-Books</span>
+  </div>
+</nav>
+    </div>
+  </div>
     <div class="navba">
 <nav class="navbar navbar-expand-lg navbar-">
   <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../assets/h.png"  style="width:50px;" > 
+    </a>
     <div class="collapse navbar-collapse" id="navbarScroll">
+      <div class="anchura">
       <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <input class="form-control me-2" type="search" placeholder="" aria-label="">
+        <button class="btn btn-outline-light" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+</svg></button>
       </form>
-      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+      </div>
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 200px;">
         
-<li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+       <div class="cate">
+        <div class="btn-group text-end">
+          <button class="btn btn-light  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Categorías
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Acción</a></li>
+            <li><a class="dropdown-item" href="#">Aventura</a></li>
+            <li><a class="dropdown-item" href="#">Romance</a></li>
+            <li><a class="dropdown-item" href="#">Ficción</a></li>
+            <li><a class="dropdown-item" href="#">Tecnología</a></li>
+            <li><a class="dropdown-item" href="#">Drama</a></li>
+            <li><a class="dropdown-item" href="#">Ciencia</a></li>
+          </ul>
+        </div>
+       </div>
+<!-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown"
+           role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Categorías
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Acción</a></li>
+            <li><a class="dropdown-item" href="#">Aventura</a></li>
+            <li><a class="dropdown-item" href="#">Romance</a></li>
+            <li><a class="dropdown-item" href="#">Ficción</a></li>
+            <li><a class="dropdown-item" href="#">Tecnología</a></li>
+            <li><a class="dropdown-item" href="#">Drama</a></li>
+            <li><a class="dropdown-item" href="#">Ciencia</a></li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Link</a>
-        </li>
+        </li>-->
       </ul>
-      
+      <i class="bi bi-person-circle"></i>
     </div>
-  </div>
+  </div> 
   
 </nav>
   </div>
@@ -45,20 +78,32 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 .navba {
  background-color: #320000;
  
 }
 
+.nav {
+  background-color:#ccab80;
+}
+
+.anchura {
+width: 720px;
+}
+.altura {
+height: 40px;
+}
+.cate {
+  position: absolute;
+  top: 18px;
+  right: 300px;
+  
+}
+.cate {
+  position: absolute;
+  top: 18px;
+  right: 300px;
+  
+}
 </style>
