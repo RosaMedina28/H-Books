0c47c3bf8a5af247444c7c9fbfeba14d410f33d7
angel zapata <angelzapata582@gmail.com>
2022-03-10 17:57:35 -0600

    importacion de index de rutas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from '../src/components/router/index.js'
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
