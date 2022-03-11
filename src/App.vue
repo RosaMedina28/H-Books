a0d43dd87cd0a0418fe13151aae638798d777bb7
angel zapata <angelzapata582@gmail.com>
2022-03-10 18:02:16 -0600

    agregado del nav, footer y componentes dinamicos en el archivo app; agregado de tipografia de forma global

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,58 @@
 <template>
-<div>
-  <div id="nav"></div>
-  <div id="main"></div>
-  <div id="footer"></div>
-</div>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div><navbarc/></div>
+    <div><router-view></router-view></div>
+    <div><footerc/></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import router from './components/router/index.js'
+import navbarc from "./components/Vue/Navbar-component.vue";
+//import mainpage from "./components/Vue/MainPage.vue";
+import footerc from "./components/Vue/Footer-component.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    navbarc,
+    //mainpage,
+    footerc,
+  },
+  mounted(){
+    //router.push('/home')
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+@font-face{
+  font-family: "Rochester";
+  unicode-range:U+000-5FF;
+  src: local('Rochester'), url('@/assets/fonts/Rochester/Rochester-Regular.ttf') format('truetype');
+}
+@font-face{
+  font-family:"Overpass";
+  unicode-range: U+000-5FF;
+  src: local('Overpass'), url('@/assets/fonts/Overpass/Overpass-VariableFont_wght.ttf') format('truetype');
+}
+
+.title-main {
+    font-family: "Rochester", Helvetica, Arial, Bold;
+    font-size: 48px;
+}
+.letter-page{
+  margin: .25rem;
+  font-family: "Overpass";
+  font-size: 14px;
+}
+.button-fav{
+  border: 3px solid #320000;
+  color: #320000;
 }
 </style>
