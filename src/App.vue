d7721ccba19850e9e3e5aa25e6dd45793045e990
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2022-03-13 17:08:57 -0600

    componentes admin-libro

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    
   }
 }
 </script>
@@ -20,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,7 +19,7 @@
       <div class="anchura">
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="" aria-label="">
-        <button class="btn btn-outline-light" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+        <button class="btn btn-outline-light" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search animate__rotateIn" viewBox="0 0 16 16">
   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
 </svg></button>
       </form>
@@ -107,3 +107,4 @@ height: 40px;
   
 }
 </style>
+
