fff5834f52642b5bbb81b078f48ed550c8bceb71
angel zapata <19170166@uttcampus.edu.mx>
2022-03-25 18:44:59 -0600

    separacion de main, login y registro

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,8 @@
 <template>
   <div>
-    <div><navbarc/></div>
     <div><router-view></router-view></div>
-    <div><footerc/></div>
   </div>
 </template>
-
-<script>
-//import router from './components/router/index.js'
-import navbarc from "./components/Vue/Navbar-component.vue";
-//import mainpage from "./components/Vue/MainPage.vue";
-import footerc from "./components/Vue/Footer-component.vue";
-
-export default {
-  name: "App",
-  components: {
-    navbarc,
-    //mainpage,
-    footerc,
-  },
-  mounted(){
-    //router.push('/home')
-  },
-};
-</script>
 
 <style>
 #app {
