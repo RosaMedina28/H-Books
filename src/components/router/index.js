39ee0327ded187d2bd1abfcc01db4b981bf18356
angel zapata <19170166@uttcampus.edu.mx>
2022-04-03 13:56:57 -0500

    agregado de guard en la ruta del perfil

--- src/components/router/index.js
+++ src/components/router/index.js
@@ -18,36 +18,49 @@ import ListarCategorias from '../admin/Categorias/ListarCategorias.vue';
 import InsertarCategoria from '../admin/Categorias/InsertarCategoria.vue';
 import EditarCategoria from '../admin/Categorias/EditarCategoria.vue';
 
+import VueCookies from 'vue-cookies'
+
 const router = createRouter({
     //history: createWebHistory(process.env.BASE_URL),
     history: createWebHistory(),
-    routes:[
-        {path: '/:catchAll(.*)',component:NotFound},
-        {path: '/',component:MainC,children:[
-            {path:'',component:Main,
-            beforeEnter(){
-                if(!localStorage.getItem('token')){
-                    localStorage.setItem('token',"1|l72J1a3CXt7r3TykHerWqdWfvg4vZKZvxRejPHHB")
-                    localStorage.setItem('rol','user')
-                    return {path: '/login'}
-                }
-            }},
-            {path: 'categoria/:category',component:category},
-            {path: 'libro/:book',component:bookseccion},
-            {path: 'perfil/:id',component:profile},
-            //{path: 'crud/libros/edit',component:listarLibros},
-        ]},
-        {path: '/new/book',component:InsertarLibro},
-        {path: '/show/books',component:listarlibros},
-        {path: '/show/category',component:ListarCategorias},
-        {path: '/new/category',component:InsertarCategoria},
-        {path: '/edit/categoria/:id',component:EditarCategoria, name:'editar_categoria'},
-        {path: '/edit/book/:id',component:ActualizarLibro, name:'editar_libro'},
+    routes: [
+        { path: '/:catchAll(.*)', component: NotFound },
+        {
+            path: '/', component: MainC, children: [
+                { path: '', name:'home',component: Main },
+                { path: 'categoria/:category', component: category },
+                { path: 'libro/:book', component: bookseccion },
+                { path: 'perfil/:id', component: profile, beforeEnter() {
+                    let user  = VueCookies.get('user')
+                    if (!user) {
+                        return{path:'login'}
+                    } else if (user) {
+                        return true
+                    }
+                } },
+                //{path: 'crud/libros/edit',component:listarLibros},
+            ]
+        },
+        { path: '/new/book', component: InsertarLibro },
+        { path: '/show/books', component: listarlibros },
+        { path: '/show/category', component: ListarCategorias },
+        { path: '/new/category', component: InsertarCategoria },
+        { path: '/edit/categoria/:id', component: EditarCategoria, name: 'editar_categoria' },
+        { path: '/edit/book/:id', component: ActualizarLibro, name: 'editar_libro' },
 
-        {path:'/',component:App,children:[
-            {path: '/login',component:login,name:login},
-            {path:'/registro',component:registerpage},
-        ]},
+        {
+            path: '/', component: App, children: [
+                { path: '/login', component: login, name: login,beforeEnter(){
+                    const user = VueCookies.get('user');
+                    if(user){
+                        return {path: '/'}
+                    }else{
+                        return true
+                    }
+                } },
+                { path: '/registro', component: registerpage },
+            ]
+        },
     ]
 })
 export default router
