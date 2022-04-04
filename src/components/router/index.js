945e0aca143aa05a119e12fa1b3785053ac588d7
angel zapata <19170166@uttcampus.edu.mx>
2022-04-04 15:12:08 -0500

    cambio de las rutas

--- src/components/router/index.js
+++ src/components/router/index.js
@@ -20,45 +20,63 @@ import EditarCategoria from '../admin/Categorias/EditarCategoria.vue';
 
 import VueCookies from 'vue-cookies'
 
+import ListarCitas from '../admin/Citas/ListarCita.vue';
+import ActualizarCitas from '../admin/Citas/ActualizarCita.vue';
+import InsertarCitas from '../admin/Citas/InsertarCita.vue';
+
 const router = createRouter({
     //history: createWebHistory(process.env.BASE_URL),
     history: createWebHistory(),
     routes: [
-        { path: '/:catchAll(.*)', component: NotFound },
         {
             path: '/', component: MainC, children: [
-                { path: '', name:'home',component: Main },
+                { path: '/:catchAll(.*)', component: NotFound },
+                { path: '', name: 'home', component: Main },
                 { path: 'categoria/:category', component: category },
                 { path: 'libro/:book', component: bookseccion },
-                { path: 'perfil/:id', component: profile, beforeEnter() {
-                    let user  = VueCookies.get('user')
-                    if (!user) {
-                        return{path:'login'}
-                    } else if (user) {
-                        return true
+                {
+                    path: 'perfil/:id', component: profile, beforeEnter() {
+                        let user = VueCookies.get('user')
+                        if (!user) {
+                            return { path: 'login' }
+                        } else if (user) {
+                            return true
+                        }
                     }
-                } },
+                },
+                { path: '/new/book', component: InsertarLibro, name:'insertar_libro'},
+                { path: '/show/books', component: listarlibros, name: 'listar_libros'},
+                { path: '/show/category', component: ListarCategorias, name: 'listar_categorias'},
+                { path: '/show/cita', component: ListarCitas, name: 'listar_citas'},
+                { path: '/new/category', component: InsertarCategoria, name: 'insertar_categoria'},
+                { path: '/new/cita', component: InsertarCitas, name: 'insertar_citas'},
+                { path: '/edit/categoria/:id', component: EditarCategoria, name: 'editar_categoria' },
+                { path: '/edit/book/:id', component: ActualizarLibro, name: 'editar_libro' },
+                { path: '/edit/cita/:id', component: ActualizarCitas, name: 'editar_cita' },
                 //{path: 'crud/libros/edit',component:listarLibros},
             ]
         },
-        { path: '/new/book', component: InsertarLibro },
-        { path: '/show/books', component: listarlibros },
-        { path: '/show/category', component: ListarCategorias },
-        { path: '/new/category', component: InsertarCategoria },
-        { path: '/edit/categoria/:id', component: EditarCategoria, name: 'editar_categoria' },
-        { path: '/edit/book/:id', component: ActualizarLibro, name: 'editar_libro' },
 
         {
             path: '/', component: App, children: [
-                { path: '/login', component: login, name: login,beforeEnter(){
+                {
+                    path: '/login', component: login, name: login, beforeEnter() {
+                        const user = VueCookies.get('user');
+                        if (user) {
+                            return { path: '/' }
+                        } else {
+                            return true
+                        }
+                    }
+                },
+                { path: '/registro', component: registerpage,beforeEnter() {
                     const user = VueCookies.get('user');
-                    if(user){
-                        return {path: '/'}
-                    }else{
+                    if (user) {
+                        return { path: '/' }
+                    } else {
                         return true
                     }
                 } },
-                { path: '/registro', component: registerpage },
             ]
         },
     ]
