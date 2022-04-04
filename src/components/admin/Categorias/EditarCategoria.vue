379cee66d9a7cb20afa45425627b5984527ec60d
angel zapata <19170166@uttcampus.edu.mx>
2022-04-04 15:11:25 -0500

    agregado de funcionalidad de categorias en vistas de admin

--- src/components/admin/Categorias/EditarCategoria.vue
+++ src/components/admin/Categorias/EditarCategoria.vue
@@ -1,48 +1,143 @@
 <template>
-        <br>
-    <div class="container">
-        <div>
-            <h2 class="brown">Editar Categoria</h2>
-        </div>
-        <br>
-        <div class="row text-start justify-content-center" >
-            <div class="col-lg-6 bg-form-2">
-                <form class="white" style="margin-top: 30px;margin-bottom: 30px">
-                    <div class="mb-3">
-                        <label  class="form-label">Nombre</label>
-                        <input type="text" class="form-control" placeholder="Aventura">
-                    </div>
-                    <br>
-                    <div class="d-grid gap-2 ">
-                        <button type="submit" class="btn white bg-form-1 d-grid gap-2" style="">Editar Categoria</button>
-                    </div>
-                </form>
-            </div>
-        </div>
+  <br />
+  <div class="container mb-4">
+    <Transition name="bounce">
+      <div id="placeholderAlert"></div>
+    </Transition>
+    <div>
+      <h2 class="brown">Editar Categoria</h2>
     </div>
+    <br />
+    <div class="row text-start justify-content-center">
+      <div class="col-lg-6 bg-form-2">
+        <form
+          @submit.prevent="putCategoria()"
+          method="PUT"
+          class="white"
+          style="margin-top: 30px; margin-bottom: 30px"
+        >
+          <div class="mb-3">
+            <label class="form-label">Nombre</label>
+            <input
+              v-model="nombre"
+              type="text"
+              class="form-control"
+              placeholder="categoria"
+            />
+          </div>
+          <br />
+          <div class="d-grid gap-2">
+            <button
+              type="submit"
+              id="alertbtn"
+              class="btn white bg-form-1 d-grid gap-2"
+            >
+              Editar Categoria
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import VueCookies from "vue-cookies";
 export default {
-    name: 'EditarLibro',
-    props: {
-        msg: String
+  name: "EditarLibro",
+  data() {
+    return {
+      edit: false,
+      id: 0,
+      nombre: "",
+      categoria: "",
+      token: "",
+    };
+  },
+  methods: {
+    putCategoria() {
+      axios
+        .put(
+          "http://localhost:8000/api/actualizar/categoria",
+          {
+            categoria_id: this.id,
+            nombre: this.nombre,
+          },
+          {
+            headers: {
+              Authorization: "Bearer " + this.token,
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods":
+                "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+              "Access-Control-Allow-Headers":
+                "Origin, Content-Type, X-Auth-Token",
+            },
+          }
+        )
+        .then(() => {
+          var alertPlaceholder = document.getElementById("placeholderAlert");
+
+          function alert(message, type) {
+            var wrapper = document.createElement("div");
+            wrapper.innerHTML =
+              '<div class="alert alert-' +
+              type +
+              ' alert-dismissible" role="alert">' +
+              message +
+              '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+
+            alertPlaceholder.append(wrapper);
+          }
+          alert("Categoria Editada Correctamente", "primary");
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+  mounted() {
+    if (this.$route.params.nombre) {
+      this.id = this.$route.params.id;
     }
-}
+    if (this.$route.params.id) {
+      this.nombre = this.$route.params.nombre;
+    }
+    if (VueCookies.get("user")) {
+      this.token = VueCookies.get("user").token;
+    }
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.bg-form-2{
-    background-color: #320000;
+.bg-form-2 {
+  background-color: #320000;
 }
-.brown{
-    color: #320000;
+.brown {
+  color: #320000;
 }
-.white{
-    color: #ffff;
+.white {
+  color: #ffff;
 }
-.bg-form-1{
-    background-color: #CCAB80;
+.bg-form-1 {
+  background-color: #ccab80;
+}
+
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
--- src/components/admin/Categorias/ListarCategorias.vue
+++ src/components/admin/Categorias/ListarCategorias.vue
@@ -1,69 +1,126 @@
 <template>
-    <div class="container">
-        <div class="row text-center">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Nombre</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody v-if="categorias.length > 0">
-                    <tr v-for="(categoria, index) in categorias" :key="index">
-                        <td>{{ categoria.id }}</td>
-                        <td>{{ categoria.nombre }}</td>
-                        <td>
-                            <button type="button" @click="deleteCategoria(categoria.id)" class="btn btn-danger btn-block">Eliminar</button>
-                            
-                            <router-link :to='{name:"editar_categoria",params:{id:categoria.id}}' class="btn btn-warning btn-block" style="margin-left:20px;">Editar</router-link>
-                        </td>
-                    </tr>
-                </tbody>
-                <tbody v-else>
-                    <tr>
-                        <td colspan="4" align="center">No hay categorias registradas.</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+  <div class="container">
+    <Transition name="bounce">
+      <div id="placeholderAlert"></div>
+    </Transition>
+    <div class="d-flex justify-content-end">
+        <router-link class="btn btn-primary my-2" :to='{name:"insertar_categoria"}'>Agregar +</router-link>
     </div>
+    <div class="row text-center">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody v-if="categorias.length > 0">
+          <tr v-for="(categoria, index) in categorias" :key="index">
+            <td>{{ categoria.id }}</td>
+            <td>{{ categoria.nombre }}</td>
+            <td>
+              <button
+                type="button"
+                @click="deleteCategoria(categoria.id)"
+                class="btn btn-danger btn-block"
+              >
+                Eliminar
+              </button>
+
+              <router-link
+                :to="{
+                  name: 'editar_categoria',
+                  params: { id: categoria.id, nombre: categoria.nombre },
+                }"
+                class="btn btn-warning btn-block"
+                style="margin-left: 20px"
+                >Editar</router-link
+              >
+            </td>
+          </tr>
+        </tbody>
+        <tbody v-else>
+          <tr>
+            <td colspan="4" align="center">No hay categorias registradas.</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-    
-    data(){
-        return {
-            categorias:[]
-        }
+  data() {
+    return {
+      categorias: [],
+    };
+  },
+  mounted() {
+    this.getCategorias();
+  },
+  methods: {
+    async getCategorias() {
+      await axios
+        .get("http://127.0.0.1:8000/api/listar/categorias")
+        .then((response) => {
+          this.categorias = response.data.categorias;
+          console.log(response.data.categorias);
+        })
+        .catch((error) => {
+          console.log(error);
+          this.categorias = [];
+        });
     },
-    mounted(){
-        this.getCategorias()
-    },
-    methods:{
-        async getCategorias(){
-            await axios.get('http://127.0.0.1:8000/api/listar/categorias').then(response=>{
-                this.categorias = response.data.categorias
-                console.log(response.data.categorias)
-            }).catch(error=>{
-                console.log(error)
-                this.categorias = []
-            })
-        },
-        deleteCategoria(id){
-            if(confirm("Estas seguro de eliminar esta categoria?")){
-                axios.post('http://127.0.0.1:8000/api/eliminar/categoria',{
-                    categoria_id:id
-                }).then(response=>{
-                    this.getCategorias()
-                    console.log(response)
-                }).catch(error=>{
-                    console.log(error)
-                })
+    deleteCategoria(id) {
+      if (confirm("Estas seguro de eliminar esta categoria?")) {
+        axios
+          .post("http://127.0.0.1:8000/api/eliminar/categoria", {
+            categoria_id: id,
+          })
+          .then(() => {
+            this.getCategorias();
+            var alertPlaceholder = document.getElementById("placeholderAlert");
+
+            function alert(message, type) {
+              var wrapper = document.createElement("div");
+              wrapper.innerHTML =
+                '<div class="alert alert-' +
+                type +
+                ' alert-dismissible" role="alert">' +
+                message +
+                '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+
+              alertPlaceholder.append(wrapper);
             }
-        }
-    }
-}
+            alert("Categoria Eliminada Correctamente", "danger");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+  },
+};
 </script>
+<style scoped>
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
